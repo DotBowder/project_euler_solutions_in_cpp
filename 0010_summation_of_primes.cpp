aee884ec7664002003534b82e8400a8c92cc9f11
Daniel Bowder <D.Bowder@gmail.com>
2018-07-26 12:57:44 -0700

    Update 0010_summation_of_primes.cpp

--- 0010_summation_of_primes.cpp
+++ 0010_summation_of_primes.cpp
@@ -29,9 +29,15 @@ bool isPrime(double i) {
   return true;
 }
 
-double summation_of_primes(double value) {
+long long int summation_of_primes(int value) {
   // Initialize a variable to hold our sum of primes.
-  int sum = 0;
+  long long int sum = 0;
+
+  // Setup Percent Print Helper Variables
+  bool currentPercentPrint = false;
+  int currentPercent = 0;
+  int newPercent = 0;
+
   // For each number up to our value,
   for (int i=2;i<value;i++){ // Start at 2, because 1 doesn't count as prime.
     // check and see if that number is prime.
@@ -40,11 +46,18 @@ double summation_of_primes(double value) {
       // Add the number to our sum of primes.
       sum = sum + i;
     }
+
+
+    newPercent = (int)(i/(double)value*100);
+    if (newPercent != currentPercent) {
+      cout << newPercent << "% complete..." << endl;
+      currentPercent = newPercent;
+    }
   }
   // Return the sum of all of our prime numbers up to the value specified.
   return sum;
 }
 
 int main(int argc, char* argv[]) {
-  cout << "Problem . " << summation_of_primes(10) <<  endl;
+  cout << "Problem 10. " << summation_of_primes(2000000) <<  endl;
 }
